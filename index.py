a2b99ddad3594bcbc13b5d2f82c50a9038e94438
Jannik Winkel <jannik.winkel@kiney.de>
2016-09-09 22:25:37 +0200

    progress...

--- index.py
+++ index.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print('hellö')
+#some config stuff
+#SERVER = 'gevent'
+SERVER = 'wsgiref'
+URLPREFIX = 'http://home.kiney.de/ds-anmeldung'
+DEBUGMODE = True
+DBURL = 'sqlite:///ltb.db'
 
+#gevent monkey patching MUST happen before other imports
+if SERVER == 'gevent' and __name__ == '__main__':
+    from gevent import monkey; monkey.patch_all()
+
+from ltb import LtbDB
+
+from bottle import get
+import bottle
+
+ltbdb = LtbDB(DBURL)
+
+@get('/')
+def index():
+    return str(ltbdb.get_all_ltbs())
+
+
+if __name__ == '__main__':
+    #logging.basicConfig(filename='signup.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S %Z')
+    bottle.debug(DEBUGMODE)
+    bottle.run(host='127.0.0.1', port=8080, server=SERVER)
--- ltb.py
+++ ltb.py
@@ -1,28 +1,72 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# ausprobiert... erstmal cool, aber suckt überlst bei multi-statments, transactions gehen garnicht
-# außer vllt. mit einerm dieser kickass pull-requests:
+# records is missing native transaction support, there are pulll-requests:
 # https://github.com/kennethreitz/records/pull/58
 # https://github.com/kennethreitz/records/pull/75
+# work-around:
+# tx = db.db.begin() / tx.commit()
+# i guess all of this is not really thread safe!
 import records
 #import sqlalchemy
 import enum
 
 
-LOC = enum.Enum('Location', 'unknown Hannover Marburg Harz')
+#LOC = enum.Enum('Location', 'unknown Hannover Marburg Harz')
+DBURL = 'sqlite:///ltb.db'
 
 
-class LTB():
-    '''
-    LTB class
-    '''
-    def __init__(self, nr, title, present=False, location=LOC.unknown)
+#class LTB(): # FIXME useful?
+    #'''
+    #LTB class
+    #'''
+    #def __init__(self, nr, title, present=False, location=0):
+        #pass
 
 class LtbDB():
     '''
     database interface class
     '''
-    def __init__(self):
-        print('TODO')
+    def __init__(self, dburl=DBURL):
+        self._dburl = dburl
+        self.db = records.Database(dburl) # db handle, only for reading because of thread saferty
+        self.init_location_ids()
+    
+    def init_location_ids(self):
+        '''
+        create location mappings for fast lookup
+        (forward and backward)
+        '''
+        lid = {}
+        rlid = {}
+        r = self.db.query('SELECT id, name FROM locations;')
+        for i in r:
+            lid[i[0]] = i[1]
+            rlid[i[1]] = i[0]
+        self.lid = lid
+        self.rlid = rlid
+    
+    def get_ltb_by_id(self, ltbid):
+        q ='''SELECT ltbs.ltbid, ltbs.title, locations.name AS location, ltbs.dupes, ltbs.present
+FROM ltbs
+INNER JOIN locations ON ltbs.location = locations.id
+WHERE ltbs.ltbid = :ltbid;
+'''
+        ltb = self.db.query(q, ltbid = ltbid)[0].as_dict()
+        return ltb
+    
+    def get_all_ltbs(self):
+        q = '''SELECT ltbs.ltbid, ltbs.title, locations.name AS location
+FROM ltbs
+INNER JOIN locations ON ltbs.location = locations.id'''
+        a = self.db.query(q)
+        ltbs = list(map(dict, list(a)))
+        return ltbs
+        
 
+if __name__ == '__main__':
+    l = LtbDB(DBURL)
+    print(l.lid)
+    print(l.rlid)
+    print(l.get_ltb_by_id(256))
+    #print(l.get_all_ltbs())
